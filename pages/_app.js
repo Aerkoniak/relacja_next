f223848a025db887711c0f963fa0235280dc9cbb
Michal Szczepanski <lunnier@gmail.com>
2020-11-11 19:11:27 +0100

    Container and Header implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+
+import Header from '../components/Header';
+import MainHead from '../components/MainHead';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Container fluid>
+      <Header />
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default MyApp
