fd11d5ec2b5b0371b8fe25154169fb3d11ed8425
Michal Szczepanski <lunnier@gmail.com>
2020-11-18 13:17:05 +0100

    about and therapy pages added

--- pages/o-mnie.js
+++ pages/o-mnie.js
@@ -1,10 +1,31 @@
-import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+import { Container, Row, Col } from 'react-bootstrap';
+
+import homeStyles from '../styles/Home.module.css';
+import pageStyles from '../styles/Pages.module.css';
 
 
 function About() {
     return (
-        <section className={styles.main}>
-            <h3 id="o-mnie">o mnie</h3>
+        <section className={homeStyles.main} id="o-mnie">
+            <Head>
+                <title>Re-Lacja - O mnie</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Container fluid>
+                <Row >
+                    <Col md={{ offset: 1, span: 6 }}><p className={pageStyles.landing}>Jestem absolwentką Uniwersytetu Mikołaja Kopernika w Toruniu, na którym uzyskałam dyplom magistra pedagogiki o specjalności pedagogika szkolna z socjoterapią i terapią pedagogiczną. Ukończyłam również dwuletnie Studium Socjoterapii i Treningu Interpersonalnego w Krakowskim Centrum Psychodynamicznym  oraz jestem w trakcie studiów w Szkole Psychoterapii Psychodynamicznej rekomendowanej przez Polskie Towarzystwo Psychologiczne. Swoją wiedzę staram się systematycznie uzupełniać i poszerzać, biorąc udział w licznych konferencjach, szkoleniach oraz seminariach.</p> </Col>
+                    <Col md={{ span: 5 }}><img src="/PTP.png" alt="" className={pageStyles.imagePTP} /></Col>
+                </Row>
+                <Row className={pageStyles.secondRow}>
+                    <Col className={pageStyles.column} md={{ offset: 1, span: 6 }}><p className={pageStyles.landing}>Doświadczenie zawodowe zdobyłam między innymi w Wojewódzkim Szpitalu dla Nerwowo i Psychicznie Chorych im. dra J. Bednarza w Świeciu, Poradni Zdrowia Psychicznego w Brodnicy. Posiadam również doświadczenie w prowadzeniu grup terapeutycznych dla dzieci i młodzieży. Na co dzień pracuję w Centrum Medycznym we niewielkim mieście, gdzie przyjmuję pacjentów w ramach Poradni Zdrowia Psychicznego oraz prowadzę zajęcia na Oddziale Dziennym Psychiatrycznym. Od kilku lat prowadzę również indywidualną psychoterapię dla dorosłych oraz młodzieży, stosując podejście psychodynamiczne.</p></Col>
+                    <Col md={{ span: 5 }}> <img src="/KCP_trans_small.png" alt="" className={pageStyles.image} /></Col>
+                </Row>
+                <Row className={homeStyles.secondRow}>
+                    <Col className={pageStyles.column} md={{ offset: 1, span: 6 }}><p className={pageStyles.landing}>W trosce o to, aby rzetelnie wykonywać zadania psychoterapeuty, swoją pracę terapeutyczną regularnie poddaję superwizji u certyfikowanego psychoterapeuty Polskiego Towarzystwa Psychoterapii Psychodynamicznej. Co więcej, w swojej praktyce kieruję się zasadami zawartymi w Kodeksie Etyki Psychoterapeuty.</p></Col>
+                    <Col md={{ span: 5 }}> <img src="/umk-small.png" alt="" className={pageStyles.image} /></Col>
+                </Row>
+            </Container>
         </section>
     )
 }
--- pages/o-terapiach.js
+++ pages/o-terapiach.js
@@ -1,10 +1,25 @@
-import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+import homeStyles from '../styles/Home.module.css'
+import pageStyles from '../styles/Pages.module.css'
+
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 
 function Therapies() {
     return (
-        <section className={styles.main}>
-            <h3>o terapiach</h3>
+        <section className={homeStyles.main} id="o-terapiach">
+            <Head>
+                <title>Re-Lacja - O terapiach</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Container fluid>
+                <Row>
+                    <Col className={pageStyles.secondRow} md={{ offset: 2, span: 8 }}><p className={pageStyles.landing}>Teoretyczną bazą podejścia psychodynamicznego jest psychoanaliza, zapoczątkowana przez Zygmunta Freuda na przełomie XIX i XX wieku, rozwijana w kolejnych latach przez wielu badaczy. Myślenie psychodynamiczne jest z założenia otwarte na wszelkie nowości nie tylko z zakresu psychologii, ale także psychiatrii, niejednokrotnie wykorzystując je jako korzystne dla pacjenta. Przedstawiciele nurtu psychodynamicznego stoją na stanowisku, że życiem jednostki sterują wewnętrzne, często nieuświadomione, potrzeby oraz mechanizmy działania, nie sposób więc zrozumieć psychicznych przeżyć pacjenta bez pochylenia się nad jego nieświadomością. W tym celu niezwykle ważne jest zbudowanie więzi pomiędzy pacjentem i terapeutą, co umożliwić ma terapia prowadzona „twarzą w twarz”, podczas której niezwykle ważna jest swobodna wypowiedź pacjenta oraz jego wolne skojarzenia. Rolą terapeuty jest „podążanie za pacjentem”, aby razem z nim dotrzeć do źródła objawów.</p></Col>
+                    <Col className={pageStyles.secondRow} md={{ offset: 2, span: 8 }}><p className={pageStyles.landing}>Decydując się na psychoterapię w moim gabinecie, należy wiedzieć, że cały proces terapeutyczny rozpoczynam od sesji konsultacyjnej, umożliwiającej mi zapoznanie się z pacjentem, jego problemami oraz linią życia, co pozwala zaplanować dalszą terapię. Rzetelnie zebrany wywiad pozwala dostosować proces terapeutyczny do indywidualnych potrzeb pacjenta, a także przedstawić mu realne do osiągnięcia rezultaty.</p></Col>
+                    <Col className={pageStyles.secondRow} md={{ offset: 2, span: 8 }}><p className={pageStyles.landing}>Sesje psychoterapii odbywają się raz lub dwa razy w tygodniu i trwają po 50 minut. W przypadku dzieci i młodzieży na spotkania zapraszam rodziców lub opiekunów prawnych wraz z podopiecznym. Konsultacja trwa zazwyczaj od dwóch do czterech sesji, w których trakcie przeprowadzam rozmowę z rodzicami i poznaję dziecko. W przypadku młodzieży spotkania z opiekunami są rzadsze i stanowią jedynie możliwość wymiany – najistotniejszych z punktu widzenia przebiegu procesu terapeutycznego – informacji. Niezwykle ważne jest dla mnie zachowanie prawa do intymności adolescenta oraz poszanowanie tajemnicy sesji. Warto podkreślić, że leczenie pacjentów niepełnoletnich możliwe jest za zgodą rodziców lub opiekunów prawnych. Jednak o zgodę pytam również samego pacjenta, ponieważ to jego motywacja jest kluczowa dla całego przebiegu procesu terapeutycznego.</p></Col>
+                </Row>
+            </Container>
         </section>
     )
 }
