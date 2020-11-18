866587c61c2aa66bdc03771b0124c44be5079131
Michal Szczepanski <lunnier@gmail.com>
2020-11-18 13:15:53 +0100

    tariff page upgraded

--- pages/cennik.js
+++ pages/cennik.js
@@ -1,10 +1,38 @@
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import homeStyles from '../styles/Home.module.css'
+import pageStyles from '../styles/Pages.module.css';
+
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 
 function Tariff() {
+
+    useEffect(() => {
+        const app = document.getElementById("__next");
+        const maxScrollValue = app.offsetHeight - window.innerHeight;
+        console.log(maxScrollValue)
+        window.scrollTo(0, maxScrollValue)
+    })
+
     return (
-        <section className={styles.main}>
-            <h3>Cennik</h3>
+        <section className={homeStyles.main}>
+            <Head>
+                <title>Re-Lacja - Cennik</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Container fluid>
+                <Row>
+                    <Col md={{ offset: 3, span: 5 }}><h4>Cennik spotkań:</h4></Col>
+                    <Col md={{ offset: 4, span: 4 }}>
+                        <ul style={{ listStyle: "none" }}>
+                            <li><p className={pageStyles.landing}>konsultacja terapeutyczna pierwszorazowa: 120zł</p></li>
+                            <li><p className={pageStyles.landing}>sesja psychoterapii indywidualnej: 100zł - <strong>50 minut</strong></p></li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
         </section>
     )
 }
